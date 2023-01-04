c4f41e68eb1a9ba65c135089078fa570dee1f99a
David Rambo <davidrambo@mailfence.com>
2023-01-04 09:30:04 -0800

    Add mapping for Treemacs.

--- config.el
+++ config.el
@@ -66,6 +66,9 @@
   ;; home row priorities: 8 6 4 5 - - 1 2 3 7
   (setq avy-keys '(?t ?e ?i ?s ?r ?o ?a ?n)))
 
+(map! :leader
+      "e" #'treemacs)
+
 (setq org-directory "~/notes/")
 
 (add-hook! org-mode
