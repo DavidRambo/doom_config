479dade61f662d518311f71504e9fef9de5d3c8a
David Rambo <davrambo@gmail.com>
2023-02-22 08:57:23 -0800

    Pin transient and with-editor to work with 28.2

--- packages.el
+++ packages.el
@@ -56,3 +56,5 @@
 (package! mixed-pitch)
 (package! visual-fill-column)
 (package! counsel)
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
